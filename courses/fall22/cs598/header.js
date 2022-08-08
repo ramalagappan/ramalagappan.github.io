e3ab92c22f5363641b1d8acef5f064f8c2a68c1e
Ram Alagappan <ralagappan@vmware.com>
2022-08-08 10:53:35 -0500

    add more details for the course

---
+++ courses/fall22/cs598/header.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+	<font color="E84A27">\
+	<h1 align="center">CS 598 - Storage Systems: Theory and Practice</h1>\
+	</font>\
+	<font color=#333333>\
+	<h4 align="center">Computer Science, UIUC, Fall 2022</h4>\
+	</font>\
+	<TABLE align=center>\
+	<TR>\
+	<TD style="width:60px"> <a href="index.html"><h3>Home </h3></a></TD>\
+	<TD style="width:90px"> <a href="schedule.html"><h3>Schedule </h3></a></TD>\
+	<TD style="width:80px"> <a href="reviews.html"><h3>Reviews</h3></a></TD>\
+	<TD style="width:120px"> <a href="readings.html"><h3>Reading List </h3></a></TD>\
+	<TD style="width:90px"> <a href="project2.html"><h3>Project </h3></a></TD>\
+	</TR>\
+	</TABLE>\
+');
