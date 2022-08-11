a1468b6777b85a0c24be460d41f89ceaa705c837
Ram Alagappan <ralagappan@vmware.com>
2022-08-11 11:42:06 -0500

    course web page updates

--- courses/fall22/cs598/header.js
+++ courses/fall22/cs598/header.js
@@ -2,18 +2,23 @@ document.write('\
 \
 	<br><br>\
 	<font color="E84A27">\
-	<h1 align="center">CS 598 - Storage Systems: Theory and Practice</h1>\
+	<h1 align="center">CS 598 RAP - Storage Systems: Theory and Practice</h1>\
 	</font>\
-	<font color=#333333>\
-	<h4 align="center">Computer Science, UIUC, Fall 2022</h4>\
+	<font color=13294B>\
+	<h2 align="center">University of Illinois Urbana-Champaign</h2>\
+	<h2 align="center">Fall 2022</h2>\
 	</font>\
 	<TABLE align=center>\
 	<TR>\
-	<TD style="width:60px"> <a href="index.html"><h3>Home </h3></a></TD>\
-	<TD style="width:90px"> <a href="schedule.html"><h3>Schedule </h3></a></TD>\
-	<TD style="width:80px"> <a href="reviews.html"><h3>Reviews</h3></a></TD>\
-	<TD style="width:120px"> <a href="readings.html"><h3>Reading List </h3></a></TD>\
-	<TD style="width:90px"> <a href="project2.html"><h3>Project </h3></a></TD>\
+	<TD style="width:60px"> <a href="index.html"><h2>Home </h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:90px"> <a href="schedule.html"><h2>Schedule </h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:80px"> <a href="reviews.html"><h2>Reviews</h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:100px"> <a href="readings.html"><h2>Reading</h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:90px"> <a href="project2.html"><h2>Project </h2></a></TD>\
 	</TR>\
 	</TABLE>\
 ');
