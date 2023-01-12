d78ba2be261f0d18fe6e5e063ba6ceda1bd65d71
Ram Alagappan <ralagappan@vmware.com>
2023-01-12 11:57:39 -0600

    temp

---
+++ courses/spring23/cs598/header.js
@@ -0,0 +1,24 @@
+document.write('\
+\
+	<br><br>\
+	<font color="E84A27">\
+	<h1 align="center">CS 598 CDS - Storage Systems</h1>\
+	</font>\
+	<font color=13294B>\
+	<h2 align="center">University of Illinois Urbana-Champaign</h2>\
+	<h2 align="center">Spring 2023</h2>\
+	</font>\
+	<TABLE align=center>\
+	<TR>\
+	<TD style="width:60px"> <a href="index.html"><h2>Home </h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:90px"> <a href="schedule.html"><h2>Schedule </h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:80px"> <a href="reviews.html"><h2>Reviews</h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:100px"> <a href="readings.html"><h2>Reading</h2></a></TD>\
+	<TD style="width:10px"> <h2><font color=13294B>|</font></h2></TD>\
+	<TD style="width:90px"> <a href="project2.html"><h2>Project </h2></a></TD>\
+	</TR>\
+	</TABLE>\
+');
